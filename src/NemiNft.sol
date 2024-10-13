4293190231e58c019b65bc8bb449d3855ede277a
owanemi <owanemi1741@gmail.com>
2024-10-13 23:39:15 +0100

    create nft

---
+++ src/NemiNft.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+
+import {ERC721} from "lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+
+contract NemiNft is ERC721 {
+    
+}
